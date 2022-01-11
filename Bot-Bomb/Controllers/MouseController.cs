39b69233e09c06bab43496cf3e3226ad9a5110db
Felipe Aurélio <faurelio_s@hotmail.com>
2022-01-11 00:33:49 -0300

    Upload Project

---
+++ Bot-Bomb/Controllers/MouseController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bot_Bomb.Controllers
+{
+    public class MouseController
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public static void MoveCursor(Rectangle location)
+        {
+            Cursor.Position = new Point(location.X + location.Width / 2, location.Y + location.Height / 2);
+            //Cursor.Clip = new Rectangle(location.Location, location.Size);
+            DoMouseClick();
+        }
+        public static void DoMouseClick()
+        {
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
+        public static Boolean FindLocation(Bitmap FirstScreen, Bitmap SecondScreen)
+        {
+            Boolean resultado = true;
+            CaptureController.screen();
+            Bitmap ThreedScreen = new Bitmap(@"C:\Users\lipex\Documents\Repository\Bot-Bomb\Bot-Bomb\ScreenSave\ScreenShot.jpg", true);
+            Rectangle location2 = new Rectangle();
+            Rectangle location1 = CaptureController.searchBitmap(SecondScreen, FirstScreen, 0.5);
+            int x = 0, y = 0;
+            Console.WriteLine("X: " + location1.X + "\nY: " + location1.Y + "\nWidth: " + location1.Width + "\nHeight: " + location1.Height);
+            for (x = 0; x < ThreedScreen.Width; x++)
+            {
+                for (y = 0; y < ThreedScreen.Height; y++)
+                {
+                    if (x >= location1.X && y >= location1.Y)
+                    {
+                        if (y <= location1.Y + location1.Height)
+                        {
+                            if (x <= location1.X + location1.Width)
+                            {
+                                if (x == Convert.ToInt32((location1.X + (location1.Width / 2))) && y == Convert.ToInt32((location1.Y + (location1.Height / 2))))
+                                {
+                                    location2.X = x;
+                                    location2.Y = y;
+                                }
+                                Color pixelColor = ThreedScreen.GetPixel(x, y);
+                                Color newColor = Color.FromArgb(pixelColor.R, pixelColor.G, 0);
+                                ThreedScreen.SetPixel(x, y, newColor);
+                            }
+                        }
+                    }
+                }
+            }
+            ThreedScreen.Save(@"C:\Users\lipex\Documents\Repository\Bot-Bomb\Bot-Bomb\ScreenSave\Finish.jpg", ImageFormat.Jpeg);
+            if (location1.X == 0 && location1.Y == 0)
+            {
+                resultado = false;
+            }
+            else
+            {
+                MouseController.MoveCursor(location1);
+            }
+
+            System.IO.File.Delete(@"C:\Users\lipex\Documents\Repository\Bot-Bomb\Bot-Bomb\ScreenSave\ScreenShot.jpg");
+            System.IO.File.Delete(@"C:\Users\lipex\Documents\Repository\Bot-Bomb\Bot-Bomb\ScreenSave\Finish.jpg");
+            Console.WriteLine("Pronto");
+            return resultado;
+        }
+
+    }
+}
---
+++ Bot-Bomb/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;//                     A
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Bot_Bomb.Controllers;
+
+namespace Bot_Bomb
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            start();
+        }
+        public static void start()
+        {
+            var t = Task.Run(async delegate
+            {
+                await Task.Delay(TimeSpan.FromSeconds(10.0));
+                return 42;
+            });
+
+            int aux = 0;
+
+            Console.WriteLine("Começa em 10 segundos, coloque na tela do bomb");
+            t.Wait();
+            Console.WriteLine("Começou");
+
+            CaptureController.FocusProcess();
+            CaptureController.screen();
+            Bitmap tela = new Bitmap(@"C:\Users\lipex\Documents\Repository\Bot-Bomb\Bot-Bomb\ScreenSave\telabombHeroes.jpg", true);
+            Bitmap voltar = new Bitmap(@"C:\Users\lipex\Documents\Repository\Bot-Bomb\Bot-Bomb\ScreenSave\btnVoltar.jpg", true);
+            Bitmap hunt = new Bitmap(@"C:\Users\lipex\Documents\Repository\Bot-Bomb\Bot-Bomb\ScreenSave\treasureHunt.jpg", true);
+
+
+            while (true)
+            {
+                switch (aux)
+                {
+                    case 0:
+                        if (MouseController.FindLocation(tela, voltar))
+                        {
+                            Console.WriteLine("Achou");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Não Achou");
+                        }
+                        aux++;
+                        break;
+                    case 1:
+                        if (MouseController.FindLocation(tela, hunt))
+                        {
+                            Console.WriteLine("Achou");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Não Achou");
+                        }
+                        aux = 0;
+                        break;
+                }
+                t.Wait();
+            }
+
+
+        }
+    }
+}
+
+
